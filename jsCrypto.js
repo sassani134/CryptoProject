4b88bff8b11c796c2efe5d5e47a8db9ddd4e906e
Samuel A <sassani134@gmail.com>
2022-03-19 11:08:12 +0100

    Fin Video 2 petit proof of work

--- jsCrypto.js
+++ jsCrypto.js
@@ -6,16 +6,26 @@ class Block{
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
     }
 
     createGenesisBlock(){
@@ -28,7 +38,8 @@ class Blockchain{
 
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        //newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -50,6 +61,13 @@ class Blockchain{
 }
 
 let samCoin = new Blockchain();
+
+console.log('Mining block 1...');
+samCoin.addBlock(new Block(1," 10/03/2022", { amount: 4}));
+console.log('Mining block 2...');
+samCoin.addBlock(new Block(2," 11/03/2022", { amount: 4}));
+
+/*
 samCoin.addBlock(new Block(1," 10/03/2022", { amount: 4}));
 samCoin.addBlock(new Block(2," 11/03/2022", { amount: 4}));
 samCoin.addBlock(new Block(3," 12/03/2022", { amount: 5}));
@@ -63,3 +81,4 @@ console.log("Blockchain valide? " + samCoin.isChainValid());
 
 samCoin.chain[1].hash = samCoin.chain[1].calculateHash();
 console.log("Blockchain valide? " + samCoin.isChainValid());
+*/
