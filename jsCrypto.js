95ab8872c11438e1f055c394633bb86d8f7e29b5
Samuel A <sassani134@gmail.com>
2022-03-19 16:43:12 +0100

    Erreur sur le montant rendu qui est undefined | resté sur la vid3

--- jsCrypto.js
+++ jsCrypto.js
@@ -1,9 +1,16 @@
 const SHA256 = require('crypto-js/sha256')
+
+class Transactions{
+    constructor(fromAdress, toAdress, amount){
+        this.fromAdress = fromAdress;
+        this.toAdress = toAdress;
+        this.amount = amount;
+    }
+}
 class Block{
-    constructor(index, timestamp, data, previousHash = ""){
-        this.index = index;
+    constructor(timestamp, transactions, previousHash = ""){
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
         this.nonce = 0;
@@ -25,24 +32,49 @@ class Block{
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
-        this.difficulty = 4;
+        this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
 
     createGenesisBlock(){
-        return new Block(0, "01/03/2022", "Genesis block", "0");
+        return new Block("01/03/2022", "Genesis block", "0");
     }
 
     getLatestBlock(){
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock){
-        newBlock.previousHash = this.getLatestBlock().hash;
-        //newBlock.hash = newBlock.calculateHash();
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+    minePendingTransactions(miningRewardAddress){
+        let block = new Block(Date.now(), this.pendingTransactions);
+        block.mineBlock(this.difficulty);
+        
+        console.log("Block succefully mined");
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            new Transactions(null, miningRewardAddress, this.miningReward)
+        ];
     }
 
+    createTransaction(transactions){
+        this.pendingTransactions.push(transactions)
+    }
+
+    getBalanceOfAddress(address){
+        let balance = 0;
+
+        for(const block of this.chain){
+            for(const trans of block.transactions){
+                if(trans.fromAdress === address){
+                    balance -= trans.amount;
+                }
+                if (trans.toAdress === address) {
+                    balance += trans.amount;
+                }
+            }
+        }
+    }
     isChainValid(){
         for(let i = 1; i < this.chain.length; i++){
             const currentBlock = this.chain[i];
@@ -62,12 +94,29 @@ class Blockchain{
 
 let samCoin = new Blockchain();
 
+samCoin.createTransaction(new Transactions('addresse1', 'address2', 100));
+samCoin.createTransaction(new Transactions('addresse2', 'address1', 50));
+
+console.log("Starting the miner ...");
+
+samCoin.minePendingTransactions('samuel-address');
+console.log('\n Balance of samuel is', samCoin.getBalanceOfAddress('samuel-address'));
+
+
+console.log("Starting the miner ...");
+
+samCoin.minePendingTransactions('samuel-address');
+console.log('\n Balance of samuel is', samCoin.getBalanceOfAddress('samuel-address'));
+
+
+/*test video 2 ne pas aller trop loin dans la difficulty
 console.log('Mining block 1...');
 samCoin.addBlock(new Block(1," 10/03/2022", { amount: 4}));
 console.log('Mining block 2...');
 samCoin.addBlock(new Block(2," 11/03/2022", { amount: 4}));
+*/
 
-/*
+/*test video 1
 samCoin.addBlock(new Block(1," 10/03/2022", { amount: 4}));
 samCoin.addBlock(new Block(2," 11/03/2022", { amount: 4}));
 samCoin.addBlock(new Block(3," 12/03/2022", { amount: 5}));
@@ -81,4 +130,13 @@ console.log("Blockchain valide? " + samCoin.isChainValid());
 
 samCoin.chain[1].hash = samCoin.chain[1].calculateHash();
 console.log("Blockchain valide? " + samCoin.isChainValid());
+*/
+
+/* old mining method change from vid 3
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        //newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
 */
